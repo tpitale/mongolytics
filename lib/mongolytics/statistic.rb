a8294bc61f93c0c3c7aae108ae4d65c2f374098b
tpitale <tpitale@gmail.com>
2009-07-31 23:55:49 -0400

    use all instead of find on mongomapper

--- lib/mongolytics/statistic.rb
+++ lib/mongolytics/statistic.rb
@@ -7,11 +7,11 @@ module Mongolytics
     key :path, String
     
     def self.stats_for_path(path)
-      find(:conditions => {:path => path}).count
+      all(:conditions => {:path => path}).count
     end
 
     def self.stats_for_keys(controller, action)
-      find(:conditions => {:controller => controller.to_s, :action => action.to_s}).count
+      all(:conditions => {:controller => controller.to_s, :action => action.to_s}).count
     end
   end
 end
--- test/unit/mongolytics/statistic_test.rb
+++ test/unit/mongolytics/statistic_test.rb
@@ -4,12 +4,12 @@ module Mongolytics
   class StatisticTest < Test::Unit::TestCase
     context 'The Statistic class' do
       should "find count of stats for a given path" do
-        Statistic.expects(:find).with(:conditions => {:path => '/users/1'}).returns(stub(:count => 10201))
+        Statistic.expects(:all).with(:conditions => {:path => '/users/1'}).returns(stub(:count => 10201))
         Statistic.stats_for_path('/users/1').should == 10201
       end
 
       should "find count of stats for a given controller and action" do
-        Statistic.expects(:find).with(:conditions => {:controller => 'users', :action => 'show'}).returns(stub(:count => 11211))
+        Statistic.expects(:all).with(:conditions => {:controller => 'users', :action => 'show'}).returns(stub(:count => 11211))
         Statistic.stats_for_keys(:users, :show).should == 11211
       end
     end
